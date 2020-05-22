ff3fdbe3b59def77402bf598bd4e24bb726face0
Peter de Lange <lange@dbis.rwth-aachen.de>
2020-05-22 12:05:27 +0200

    redid most part, now a lot less load on camera
    (and script)

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
+require('dotenv').config();
 const request = require('request');
 const fs = require('fs');
-
 const { GraphQLClient } = require('graphql-request');
-const MjpegDecoder = require('mjpeg-decoder');
 
-const userMail = 'InputUser';
-const userName = 'InputUser';
-const userPw = 'Ewi_g9fTD}Nr%Xj@';
-//const backendLocation = 'https://api.indebrau.de';
-const backendLocation = 'http://localhost:4000';
-const cacheUpdateInterval = 10; // in seconds
-const camRequestInterval = 1; // in seconds
+const userMail = process.env.USERMAIL;
+const userName = process.env.USERNAME;
+const userPw = process.env.USERPW;
+const backendLocation = process.env.BACKENDLOCATION;
+const cacheUpdateInterval = process.env.CACHEUPDATEINTERVAL; // in seconds
+const camRequestInterval = process.env.CAMREQUESTINTERVAL; // in seconds
 
 var graphQLClient = new GraphQLClient(backendLocation);
-var authToken = ''; // needed for the image upload, since it uses different uploader client
+var authToken = ''; // needed for the image upload, since it uses a different uploader client
 var activeMediaStreamsCache = {}; // holds a list of active media streams
 var lastCacheUpdateTimeStamp = null; // last update of cache
 
-const mainViewDecoder = new MjpegDecoder(
-  'http://192.168.178.32/', { interval: camRequestInterval * 1000 }
-);
 var lastTimeStampMainView = new Date();
 
 async function main() {
@@ -30,19 +25,21 @@ async function main() {
   // populate the initial active media streams cache
   await updateMediaStreamsCache();
   console.log('Started!');
-  // finished startup, now start media decoders
-  mainViewDecoder.start();
+  // finished startup, now start image retrieval process
+  setInterval(uploadImage, camRequestInterval * 1000);
 }
 
 main();
 
-mainViewDecoder.on('frame', (frame) => {
+async function uploadImage() {
   if (new Date() - lastCacheUpdateTimeStamp > cacheUpdateInterval * 1000) {
     // don't wait for this, might result in 1-2 seconds delayed update (and multiple updates),
     // which is ok (in comparison to pausing the media processing)
     updateMediaStreamsCache();
   }
-  let mainViewMediaStream = null; // first set to null...
+
+  // check if update is needed
+  let mainViewMediaStream = null;
   for (let i = 0; i < activeMediaStreamsCache.length; i++) {
     if (activeMediaStreamsCache[i].mediaFilesName === 'mainView') {
       // found it
@@ -50,39 +47,61 @@ mainViewDecoder.on('frame', (frame) => {
       break;
     }
   }
-  if (!mainViewMediaStream) return; // no active stream exists
+  if (!mainViewMediaStream) return; // no active stream exists for this view
+
   // check if update is too recent
-  if (new Date() - lastTimeStampMainView >
+  if (new Date() - lastTimeStampMainView <
     mainViewMediaStream.updateFrequency * 1000
   ) {
-    // if not, upload...
-    // temporary cache the "old" last time stamp in case of failure with upload
-    let oldLastTimeStampMainView = lastTimeStampMainView;
-    lastTimeStampMainView = new Date();
-    fs.writeFileSync('tempImage.jpg', frame);
-    let formData = {
-      mediaStreamName: 'mainView',
-      mediaMimeType: 'IMAGE_JPG',
-      mediaTimestamp: lastTimeStampMainView.toJSON(),
-      mediaData: fs.createReadStream('tempImage.jpg'),
-    };
-    request.post({
-      url:`${backendLocation}/uploadMedia`,
-      headers: {
-        'Authorization':'Bearer ' + authToken
-      },
-      formData: formData
-    },function cb(err, httpResponse, identifier) {
-      if (err) {
-        lastTimeStampMainView = oldLastTimeStampMainView;
-        return console.error('Failed upload:', err);
-      }
-      // todo: check, if data was acutally stored on server side
-      // since currently, only correct upload is checked for here
-      console.log('Request send, server returned:', identifier);
-    });
+    return;
   }
-});
+  // temporary cache the "old" last time stamp in case of failure with upload
+  let oldLastTimeStampMainView = lastTimeStampMainView;
+  lastTimeStampMainView = new Date();
+
+  request.head('http://192.168.178.40', function(err){
+    if (err){
+      console.log('error querying camera: ' + err);
+      lastTimeStampMainView = oldLastTimeStampMainView;
+      return;
+    }
+    let stream = request('http://192.168.178.40');
+    stream.on('error', function(err) {
+      console.log('stream request error: ' + err );
+      lastTimeStampMainView = oldLastTimeStampMainView;
+      return;
+    }).pipe(
+      fs.createWriteStream('tempImage.jpg')
+        .on('error', function(err){
+          console.log('Stream write error: ' + err);
+          lastTimeStampMainView = oldLastTimeStampMainView;
+          return;
+        })
+    ).on('close', function() {
+      let formData = {
+        mediaStreamName: 'mainView',
+        mediaMimeType: 'IMAGE_JPG',
+        mediaTimestamp: lastTimeStampMainView.toJSON(),
+        mediaData: fs.createReadStream('tempImage.jpg'),
+      };
+      request.post({
+        url: `${backendLocation}/uploadMedia`,
+        headers: {
+          'Authorization': 'Bearer ' + authToken
+        },
+        formData: formData
+      }, function cb(err, httpResponse, identifier) {
+        if (err) {
+          lastTimeStampMainView = oldLastTimeStampMainView;
+          return console.error('Failed upload:', err);
+        }
+        // TODO: check, if data was acutally stored on server side
+        // since currently, only correct upload is checked for here
+        console.log('Request send, server returned:', identifier);
+      });
+    });
+  });
+}
 
 async function loginOrRegister() {
   console.log('Login user...');
